5e888b4e9da0e3550df0ffec2d625a9b881467f0
Ryan <ryan.j.forgie@gmail.com>
2020-07-02 20:55:16 +0100

    initial commit

---
+++ src/components/landing-bio.js
@@ -0,0 +1,74 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "@emotion/styled"
+import code from '../images/code.svg'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const OuterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  height: 78vh;
+`
+
+const Description = styled.p`
+  padding: 0;
+  margin-bottom: 1rem;
+  font-size: 1.4rem;
+`
+
+const NameHeader = styled.h1`
+  font-size: 3.5rem;
+  margin-bottom: 0;
+`
+
+const CodeIcon = styled.img`
+  height: 4rem;
+  width: 4rem;
+`
+
+const LandingBio = () => (
+  <StaticQuery
+    query={graphql`
+      query LandingSiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            subtitle
+          }
+        }
+      }
+    `}
+
+    render={data => (
+      <OuterContainer>
+        {console.log('data.allMarkdownRemark.edges :>> ', data)}
+        <Container>
+          <NameHeader>{data.site.siteMetadata.title}</NameHeader>
+          <Description>{data.site.siteMetadata.subtitle}</Description>
+          <CodeIcon src={code} alt="Laptop icon"/>
+          <Description>
+            
+          </Description>
+        </Container>
+      </OuterContainer>
+    )}
+  />
+)
+
+NameHeader.propTypes = {
+  siteTitle: PropTypes.string,
+  subtitle: PropTypes.string,
+}
+
+NameHeader.defaultProps = {
+  siteTitle: ``,
+  subtitle: ``,
+}
+
+export default LandingBio
---
+++ src/components/layout.js
@@ -0,0 +1,78 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "@emotion/styled"
+
+import Header from "./header"
+import "./layout.css"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 860px;
+  padding: 0 1.0875rem 1rem;
+  padding-top: 0;
+`
+
+const FooterLink = styled.a`
+  margin-left: 5px;
+  color: hsl(203, 87%, 34%);
+`
+
+const FooterText = styled.a`
+  margin-left: 5px;
+`
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+`
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <Content>
+          <main>{children}</main>
+          <Footer>
+            <p>
+            © {new Date().getFullYear()}, Built with
+            </p>
+            <FooterLink href="https://www.gatsbyjs.org">Gatsby</FooterLink>
+            <FooterText>
+              and using
+            </FooterText>
+            <FooterLink href="https://www.brailleinstitute.org/freefont">
+              Atkinson Hyperlegible
+            </FooterLink>
+            <FooterText>
+              font
+            </FooterText>
+          </Footer>
+        </Content>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
